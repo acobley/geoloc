4b4df58bebd740c849fd1fa60a85b6357522fc6f
Andy C <acobley@computing.dundee.ac.uk>
2012-09-26 09:23:16 +0100

    Initialise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoloc::Application.config.secret_token = '7bb7949dd9fe27f510b42fc78834557123dcd37a11345dd3fba79d529997c22e6cac19cdc4641f34aa3bcc4579749fcbad7cc3bf363e6ee845354203f4620be4'
